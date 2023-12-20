5828c29d38ae20be9ca75503c3a7c4f445c2e3bb
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2023-12-20 09:32:10 -0300

    update navbar and introduction

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,26 +1,32 @@
 import '../index.css'
 
 function Introduction() {
+    const urlGithub = "https://github.com/danielsrbueno"
+    const urlLinkedin = "https://www.linkedin.com/in/daniel-da-silva-ramos-bueno-213a58252/"
     return (
-        <div className="pt-20 w-full h-4/5 flex flex-col justify-center items-center gap-3">
-            <h1 className="text-8xl font-bold text-slate-900">
+        <div className="pt-20 w-full h-4/5 flex flex-col justify-center items-center gap-3 text-center select-none">
+            <h1 className="text-7.5xl font-bold text-slate-900">
                 Seu próximo desenvolvedor
             </h1>
-            <h1 className="text-8xl font-extrabold my-gradient-radial">
+            <h1 className="text-7.5xl font-extrabold my-gradient-radial">
                 <span>Frontend ;)</span>
             </h1>
-            <p className="text-xl font-medium text-slate-500 mt-5">
+            <p className="text-lg font-medium text-slate-500 mt-5">
                 Olá! Eu sou o Daniel Bueno, tenho 16 anos, sou desenvolvedor Frontend
             </p>
             <div className='flex gap-11 mt-6'>
-                <button className='bg-black text-white w-36 h-10 px-5 flex items-center scale-105 justify-between rounded-md font-semibold shadow-lg hover:shedow-2xl hover:bg-white hover:text-black hover:scale-110 transition-all duration-500'>
-                    <i className='pi pi-github 8text-xl'></i>
-                    <p className='mr-3 mt-0.5'>Github</p>
-                </button>
-                <button className='bg-white text-blue-700 w-36 h-10 px-5 flex items-center scale-105 justify-between rounded-md font-semibold shadow-lg hover:shedow-2xl hover:shadow-blue-700 hover:bg-blue-700 hover:text-white hover:scale-110 transition-all duration-500'>
-                    <i className='pi pi-linkedin text-xl'></i>
-                    <p className='mr-2 mt-0.5'>LinkedIn</p>
-                </button>
+                <a href={urlGithub} target='blank'>
+                    <button className='bg-black text-white w-36 h-10 px-5 flex items-center justify-between rounded-md font-semibold shadow-lg hover:shedow-2xl hover:bg-white hover:text-black hover:scale-105 transition-all duration-500'>
+                        <i className='pi pi-github 8text-xl'></i>
+                        <p className='mr-3 mt-0.5'>Github</p>
+                    </button>
+                </a>
+                <a href={urlLinkedin} target='blank'>
+                    <button className='bg-white text-blue-700 w-36 h-10 px-5 flex items-center justify-between rounded-md font-semibold shadow-lg hover:shedow-2xl hover:shadow-blue-700 hover:bg-blue-700 hover:text-white hover:scale-105 transition-all duration-500'>
+                        <i className='pi pi-linkedin text-xl'></i>
+                        <p className='mr-2 mt-0.5'>LinkedIn</p>
+                    </button>
+                </a>
             </div>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,18 @@ import '../index.css'
 
 function Navbar() {
     return (
-        <nav className="w-screen px-44 py-4 bg-zinc-500/1 fixed backdrop-blur-sm flex items-center justify-around">
-        <h1 className="font-black text-5xl my-gradient-radial font-mono">&lt;Daniel/&gt;</h1>
-        <ul className="font-medium text-lg flex gap-4 text-zinc-900">
-            <li><a href="">Sobre mim</a></li>
-            <li><a href="">Projetos</a></li>
-            <li><a href="">Contato</a></li>
-        </ul>
-    </nav>
+        <nav className="w-screen px-44 py-4 bg-zinc-500/1 fixed backdrop-blur-sm flex items-center justify-around select-none">
+            <h1 className="font-black text-5xl my-gradient-radial font-mono">
+                &lt;Daniel /&gt;
+            </h1>
+            <ul className="font-semibold text-lg flex gap-6 text-zinc-90)">
+                <li><a href="">Início</a></li>
+                <li><a href="">Sobre mim</a></li>
+                <li><a href="">Projetos</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
     )
-    
 }
     
         
