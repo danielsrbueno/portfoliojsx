a83ed81bff079f798afe381ad07845e97062baa6
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2023-12-26 12:39:44 -0300

    about me page completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './components/About'
 function App() {
     return (
         <>
-            <div className="w-screen h-screen overflow-x-hidden bg-slate-100 selection:bg-zinc-800 selection:text-zinc-50">
+            <div className="w-screen h-screen overflow-x-hidden bg-zinc-100 selection:bg-zinc-800 selection:text-zinc-50">
                 <Navbar />
                 <Introduction />
                 <About />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,20 @@
 function About() {
     return (
-        <div className="bg-zinc-400/10 w-full flex flex-col justify-around items-center text-justify my-48 py-12">
-            <h1 className="mb-6 text-4xl font-bold">Sobre mim</h1>
+        <div className="bg-zinc-400/10 w-full flex flex-col justify-around items-center text-justify my-48 py-12 relative">
+            <h1 className="mb-6 text-4xl font-bold text-slate-900 before:w-32 before:h-32 before:blur-3xl before:absolute before:translate-x-36 before:-translate-y-10 before:bg-cyan-300/50">Sobre mim</h1>
             <div className="p-11 flex justify-around items-center text-justify">
-                <div className="w-5/12 text-xl font-normal flex flex-col gap-4 scale-105 indent-3">
-                    <div className="">
+                <div className="w-5/12 text-xl font-normal flex flex-col gap-4 scale-105 indent-3 text-slate-900">
+                    <div className="before:w-60 before:h-60 before:blur-3xl before:absolute before:-translate-x-32 before:-translate-y-12 before:bg-rose-300/40">
                         <p>Meu nome é Daniel Bueno, sou apaixonado por tecnologia desde pequeno. Obcecado por evolução, busco me aprimorar a cada dia.</p>
                     </div>
                     <div className="">
-                        <p>Comecei a desenvolver e programar em Julho de 2022, me apeguei totalmente pela programação. Então me candidatei para entrar na Etec Jaraguá no curso de Desenvolvimento de Sistemas com Ensino Médio, e passei em segundo lugar. No momento estou estudando e me aperfeiçoando a cada dia.</p>
+                        <p>Comecei a desenvolver e programar em 2022, me apeguei totalmente pela programação. Então me candidatei para entrar na Etec Jaraguá no curso de Desenvolvimento de Sistemas com Ensino Médio, e felizmente passei em segundo lugar. No momento estou estudando e me aperfeiçoando a cada dia.</p>
                     </div>
                     <div className="">
                         <p>Faço projetos apenas com HTML, CSS e JS puro, mas quero explorar novas linguagens, tecnologias e frameworks. Por mais que eu esteja na área de frontend, quero me tornar full stack. Pretendo aplicar para vagas de estágio, pois nunca trabalhei nessa área.</p>
                     </div>
                 </div>
-                <div>
+                <div className="before:w-44 before:h-44 before:blur-3xl before:absolute before:translate-x-52 before:translate-y-60 before:bg-amber-300/50">
                     <img src="../public/foto-perfil.png" alt="foto de perfil" className="w-96 rounded-full drop-shadow-lg scale-110 -mt-3"/>
                 </div>
             </div>
