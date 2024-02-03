222c9ebc16877c2f5c4ab18fb1dc5f0acf0947c7
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2024-02-03 16:42:08 -0300

    projects  pop-up added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,58 +1,185 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+import { Tooltip } from 'primereact/tooltip';
 
 Card.propTypes = {
-    ptype: PropTypes.string.isRequired,
-    pimg: PropTypes.string.isRequired,
-    ptitle: PropTypes.string.isRequired,
-    ptext: PropTypes.string.isRequired,
+  ptype: PropTypes.string.isRequired,
+  pimg: PropTypes.string.isRequired,
+  ptitle: PropTypes.string.isRequired,
+  ptext: PropTypes.string.isRequired,
+  picon: PropTypes.string.isRequired,
+  pabout: PropTypes.string.isRequired,
+  pdate: PropTypes.string.isRequired,
+  psite: PropTypes.string.isRequired,
+  pgithub: PropTypes.string.isRequired,
+  ptecs: PropTypes.array.isRequired,
 };
 
-function Card({ptype, pimg, ptitle, ptext}) {
-    let size = {
-        body: " p-2 pb-3",
-        img: "",
-        btn: "w-40 h-10 rounded-xl after:hover:w-44",
-        txt: "",
-        title: "text-xl",
-        blur: "blur-3xl"
-    }
+function Card({
+  ptype,
+  pimg,
+  ptitle,
+  ptext,
+  picon,
+  pabout,
+  pdate,
+  psite,
+  pgithub,
+  ptecs,
+}) {
+  const iconReact = "devicon-react-original"
+  const iconNode = "devicon-nodejs-plain"
+  const iconExpress = "devicon-express-original"
+  const iconFigma = "devicon-figma-plain"
+  const iconHtml = "devicon-html5-plain"
+  const iconCss = "devicon-css3-plain"
+  const iconJs = "devicon-javascript-plain"
+  const array = [iconHtml, iconCss, iconJs]
 
-    if (ptype == "small")
-    {
-        size = {
-            body: "p-2 pb-3",
-            img: "",
-            btn: "w-20 h-8 rounded-lg after:hover:w-24",
-            txt: "text-sm",
-            title: "text-lg",
-            blur: "blur-2xl"
-        }
-    }
-    else {
-        size = {
-            body: " p-2 pb-3",
-            img: "",
-            btn: "w-40 h-10 rounded-xl after:hover:w-44",
-            txt: "",
-            title: "text-xl",
-            blur: "blur-3xl"
-    }}
-    
-    return(
-        <div className={`${size.body} bg-white rounded-2xl flex flex-col justify-between shadow-md hover:scale-105 hover:shadow-xl transition-all duration-500 relative overflow-hidden`}>
-            <div className={`w-11/12 h-1/3 bg-orange-500 ${size.blur} absolute -top-12 -left-16`}></div>
-            <div className={`w-11/12 h-1/3 bg-orange-500 ${size.blur} absolute -top-12 -right-16`}></div>
-            <div className={`w-11/12 h-1/3 bg-rose-500 ${size.blur} absolute top-1/4 -left-16`}></div>
-            <div className={`w-11/12 h-1/3 bg-rose-500 ${size.blur} absolute top-1/4 -right-16`}></div>
-            
-            <img className={`${size.img} rounded-xl self-center z-10`} src={pimg} alt="" />
-            <div className="flex flex-col px-2 py-3 z-0">
-                <h1 className={`${size.title} font-bold text-slate-900`}>{ptitle}</h1>
-                <p className={`${size.txt} text-slate-500 truncate`}>{ptext}</p>
+  let iTecs = ["", "", iconFigma];
+
+  if (ptecs[0] === "ReactJS") {
+    iTecs[0] = iconReact;
+  }
+
+  if (ptecs[0] === "Express") {
+    iTecs[0] = iconExpress;
+  } 
+
+  if (ptecs[1] === "NodeJS") {
+    iTecs[1] = iconNode;
+  }
+
+  if (ptecs[0] === "HTML") {
+    iTecs[0] = array[0];
+    iTecs[1] = array[1];
+    iTecs[2] = array[2];
+  }
+
+  let [visible, setVisible] = useState(false);
+  
+  let size = {
+    body: " p-2 pb-3",
+    img: "",
+    btn: "w-40 h-10 rounded-xl after:hover:w-44",
+    txt: "",
+    title: "text-xl",
+    blur: "blur-3xl",
+  };
+
+  if (ptype == "small") {
+    size = {
+      body: "p-2 pb-3",
+      img: "",
+      btn: "w-20 h-8 rounded-lg after:hover:w-24",
+      txt: "text-sm",
+      title: "text-lg",
+      blur: "blur-2xl",
+    };
+  } else {
+    size = {
+      body: " p-2 pb-3",
+      img: "",
+      btn: "w-40 h-10 rounded-xl after:hover:w-44",
+      txt: "",
+      title: "text-xl",
+      blur: "blur-3xl",
+    };
+  }
+
+  return (
+    <div
+      className={`${size.body} bg-white rounded-2xl flex flex-col justify-between shadow-md hover:scale-105 hover:shadow-xl transition-all duration-500 relative overflow-hidden`}
+    >
+      <div
+        className={`w-11/12 h-1/3 bg-orange-500 ${size.blur} absolute -top-12 -left-16`}
+      ></div>
+      <div
+        className={`w-11/12 h-1/3 bg-orange-500 ${size.blur} absolute -top-12 -right-16`}
+      ></div>
+      <div
+        className={`w-11/12 h-1/3 bg-rose-500 ${size.blur} absolute top-1/4 -left-16`}
+      ></div>
+      <div
+        className={`w-11/12 h-1/3 bg-rose-500 ${size.blur} absolute top-1/4 -right-16`}
+      ></div>
+
+      <img
+        className={`${size.img} rounded-xl self-center z-10`}
+        src={pimg}
+        alt=""
+      />
+      <div className="flex flex-col px-2 py-3 z-0">
+        <h1 className={`${size.title} font-bold text-slate-900`}>{ptitle}</h1>
+        <p className={`${size.txt} text-slate-500 truncate`}>{ptext}</p>
+      </div>
+      <Button
+        className={`${size.btn} self-center outline outline-2 outline-offset-0 outline-rose-600 font-bold text-slate-900 z-50 relative overflow-hidden delay-100 after:w-0 after:h-full after:absolute after:top-0 after:left-0 after:transition-all after:hover:left-0 after:bg-rose-600 hover:delay-100 hover:text-slate-100 after:-z-10 after:rounded-r-full select-none`}
+        label="Ver mais"
+        onClick={() => setVisible(true)}
+      />
+      <Dialog
+        className="w-10/12 h-11/12 bg-slate-100 rounded-lg p-4 shadow-2xl selection:bg-zinc-800 selection:text-zinc-50"
+        visible={visible}
+        onHide={() => setVisible(false)}
+      >
+        <div className="flex h-full w-full">
+          <div className="w-5/6 h-full p-10 flex flex-col gap-6 ">
+            <div className="flex gap-8 items-center">
+              <img
+                className="h-20"
+                src={picon}
+                alt={`icone do projeto ${ptitle}`}
+              />
+              <h1 className="font-black text-5xl font-jetbrains tracking-tight">
+                {ptitle}
+              </h1>
             </div>
-            <button className={`${size.btn} self-center outline outline-2 outline-offset-0 outline-rose-600 font-bold text-slate-900 z-50 relative overflow-hidden delay-100 after:w-0 after:h-full after:absolute after:top-0 after:left-0 after:transition-all after:hover:left-0 after:bg-rose-600 hover:delay-100 hover:text-slate-100 after:-z-10 after:rounded-r-full`} onClick={(e)=>{console.log(e)}}>Ver mais</button>
+            <p className="text-xl w-11/12 font-medium">{pabout}</p>
+            <div className="flex items-center justify-start gap-1 font-medium text-lg">
+              <i className="pi pi-calendar"></i>
+              <p>{pdate}</p>
+            </div>
+
+            <div className="flex text-3xl gap-2">
+              <i className={iTecs[0] + " st"} data-pr-tooltip={ptecs[0]}></i>
+              <i className={iTecs[1] + " nd"} data-pr-tooltip={ptecs[1]}></i>
+              <i className={iTecs[2] + " rd"} data-pr-tooltip={ptecs[2]}></i>
+              <Tooltip target=".st" mouseTrack mouseTrackLeft={10} className="px-3 py-1 rounded text-zinc-50 bg-zinc-900"></Tooltip>
+              <Tooltip target=".nd" mouseTrack mouseTrackLeft={10} className="px-3 py-1 rounded text-zinc-50 bg-zinc-900"></Tooltip>
+              <Tooltip target=".rd" mouseTrack mouseTrackLeft={10} className="px-3 py-1 rounded text-zinc-50 bg-zinc-900"></Tooltip>
+            </div>
+            <div className="flex ">
+              <div className=" justify-end flex gap-2">
+                <a href={psite} target="blank">
+                  <div className="w-40 h-10 rounded-lg after:hover:w-44 self-center outline outline-2 outline-offset-0 outline-zinc-600 font-bold text-zinc-700 z-50 relative overflow-hidden delay-100 after:w-0 after:h-full after:absolute after:top-0 after:left-0 after:transition-all after:hover:left-0 after:bg-zinc-600 hover:delay-100 hover:text-slate-100 after:-z-10 after:rounded-r-full flex items-center justify-between px-8">
+                    <i className="pi pi-external-link"></i>
+                    <p>Abrir site</p>
+                  </div>
+                </a>
+                <a href={pgithub} target="blank">
+                  <div className="w-40 h-10 rounded-lg after:hover:w-44 self-center outline outline-2 outline-offset-0 outline-black font-bold text-slate-900 z-50 relative overflow-hidden delay-100 after:w-0 after:h-full after:absolute after:top-0 after:left-0 after:transition-all after:hover:left-0 after:bg-black hover:delay-100 hover:text-slate-100 after:-z-10 after:rounded-r-full flex items-center justify-between gap-2 px-6">
+                    <i className="pi pi-github text-lg"></i>
+                    <p>Repositório</p>
+                  </div>
+                </a>
+                <Button
+                  label="Fechar"
+                  onClick={() => setVisible(false)}
+                  className="w-40 h-10 rounded-lg after:hover:w-44 outline outline-2 outline-offset-0 outline-rose-600 font-bold text-slate-900 z-50 relative overflow-hidden delay-100 after:w-0 after:h-full after:absolute after:top-0 after:left-0 after:transition-all after:hover:left-0 after:bg-rose-600 hover:delay-100 hover:text-slate-100 after:-z-10 after:rounded-r-full select-none"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="h-full w-full  flex flex-col items-center justify-center">
+            <img className="w-full rounded-lg" src={pimg} alt="" />
+          </div>
         </div>
-    )
+      </Dialog>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,122 @@
-import Card from "./Card"
+import Card from "./Card";
+import { useState, useLayoutEffect } from "react";
 
 function Projects() {
-    const urlAge = "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/age-calculator.png"
-    const urlShort ="https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/short-summary.png"
-    const urlDS = "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/ds-jaragua.png"
-    const urlCPS = "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/cps-test.png"
-    const urlFesta = "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/festa-junina.png"
+  const urlAge =
+    "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/age-calculator.png";
+  const urlShort =
+    "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/short-summary.png";
+  const urlDS =
+    "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/ds-jaragua.png";
+  const urlCPS =
+    "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/cps-test.png";
+  const urlFesta =
+    "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/festa-junina.png";
+  const iconDS = "https://ds-jaragua.vercel.app/assets/logo-7e40ab17.svg";
+  const iconShort =
+    "https://raw.githubusercontent.com/danielsrbueno/short-summary/9db44eaec84df3860d405e92d50e0f8055c99dae/public/logo.svg";
+  const iconAge = "https://res.cloudinary.com/practicaldev/image/fetch/s--U6E1tTP8--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/organization/profile_image/1390/f6c7428b-c890-4c97-a586-352a887caad6.png"
+  const iconFesta = "https://github.com/danielsrbueno/festa-junina/blob/main/fogueira.png?raw=true"
+  const iconCPS = "https://cdn1.iconfinder.com/data/icons/complete-version-1/1024/action4-512.png"
 
-    return(
-        <div className="w-full flex flex-col justify-around items-center text-justify py-12 relative">
-            <h1 className="mb-6 text-4xl font-bold text-slate-900 before:w-36 before:h-36 before:blur-3xl before:absolute before:-translate-x-24 before:-translate-y-12 before:bg-rose-300/60">Meus projetos</h1>
-            <div className="w-8/12 flex flex-col py-12 relative gap-6 justify-center items-center">
-                <div className="grid grid-flow-col gap-8 columns-2">
-                    <Card ptitle="DS Jaraguá" ptext="Veja tudo sobre o curso de desenvolvimento de sistemas!" ptype="large" pimg={urlDS}/>
-                    <Card ptitle="Short summary" ptext="Saiba o conteúdo do vídeo sem sequer entrar nele." ptype="large" pimg={urlShort}/>
-                </div>
-                <div className="grid grid-flow-col gap-6 columns-3">
-                    <Card ptitle="Age calculator" ptext="Veja sua idade precisamente!" ptype="small" pimg={urlAge}/>
-                    <Card ptitle="Festa Junina" ptext="História, comidas e receitas, tudo sobre festa Junina!" ptype="small" pimg={urlFesta}/>
-                    <Card ptitle="CPS Test" ptext="Meça seu CPS e veja sua pontuação!" ptype="small" pimg={urlCPS}/>
-                </div>
-            </div>
+  const [key, setKey] = useState(false);
+
+  useLayoutEffect(() => {
+    const div = document.querySelector("#smalls");
+    const txt = document.querySelector("#more > p");
+
+    if (key) {
+      div.classList.add("grid");
+      div.classList.remove("hidden");
+      txt.textContent = "Menos projetos";
+    } else {
+      div.classList.add("hidden");
+      txt.textContent = "Mais projetos";
+    }
+  }, [key]);
+
+  return (
+    <div className="w-full flex flex-col justify-around items-center text-justify py-12 relative">
+      <h1 className="mb-6 text-4xl font-bold text-slate-900 before:w-36 before:h-36 before:blur-3xl before:absolute before:-translate-x-24 before:-translate-y-12 before:bg-rose-300/60">
+        Meus projetos
+      </h1>
+      <div className="w-8/12 flex flex-col py-12 relative gap-6 justify-center items-center">
+        <div className="grid grid-flow-col gap-8 columns-2">
+          <Card
+            ptitle="DS Jaraguá"
+            ptext="Veja tudo sobre o curso de desenvolvimento de sistemas!"
+            ptype="large"
+            pimg={urlDS}
+            pabout="Desenvolvido em parceria com dois amigos da escola, com o objetivo de promover nosso curso e ampliar nossa visibilidade, essa ideia foi sugerida por um professor, e nós decidimos assumir a responsabilidade pelo projeto. Este foi meu primeiro projeto em desenvolvimento com ReactJS!"
+            pdate="Novembro, 2023"
+            psite="https://ds-jaragua.vercel.app/"
+            pgithub="https://github.com/freitassdev/ds-etecjaragua"
+            picon={iconDS}
+            ptecs={["ReactJS", "NodeJS", "Figma"]}
+          />
+          <Card
+            ptitle="Short Summary"
+            ptext="Saiba o conteúdo do vídeo sem sequer entrar nele."
+            ptype="large"
+            pimg={urlShort}
+            picon={iconShort}
+            pabout="O Short Summary permite ao usuário visualizar um resumo do vídeo sem precisar assisti-lo, utilizando inteligência artificial para extrair o texto e criar o resumo. Essa foi minha primeira experiência com Express, a qual me proporcionou muitos aprendizados sobre NodeJS."
+            pdate="Outubro, 2023"
+            psite="https://short-summary-nu.vercel.app/"
+            pgithub="https://github.com/danielsrbueno/short-summary"
+            ptecs={["Express", "NodeJS", "Figma"]}
+          />
         </div>
-    )
+        <div className="grid grid-flow-col gap-6 columns-3" id="smalls">
+          <Card
+            ptitle="Age calculator"
+            ptext="Veja sua idade precisamente!"
+            ptype="small"
+            pimg={urlAge}
+            picon={iconAge}
+            pabout="O Age Calculator é uma aplicação web que permite ao usuário calcular sua idade em anos, meses e dias. Este projeto me permitiu aprender muito sobre a manipulação de datas em JavaScript, além de melhorar minhas habilidades com forms no HTML e CSS."
+            pdate="Julho, 2023"
+            psite="https://danielsrbueno.github.io/age-calculator/"
+            pgithub="https://github.com/danielsrbueno/age-calculator"
+            ptecs={["HTML", "CSS", "JavaScript"]}
+          />
+          <Card
+            ptitle="Festa Junina"
+            ptext="História, comidas e receitas, tudo sobre festa Junina!"
+            ptype="small"
+            pimg={urlFesta}
+            picon={iconFesta}
+            pabout="O Festa Junina é uma aplicação web que permite ao usuário conhecer mais sobre a festa junina, suas origens, comidas e receitas. Este projeto foi um trabalho escolar, e foi desenvolvido apenas para resonsividade de desktop. Ele me ajudou a entender melhor o funcionamento do JavaScript e a importância de um design minimalista."
+            pdate="Junho, 2023"
+            psite="https://danielsrbueno.github.io/festa-junina/"
+            pgithub="https://github.com/danielsrbueno/festa-junina"
+            ptecs={["HTML", "CSS", "JavaScript"]}
+          />
+          <Card
+            ptitle="CPS Test"
+            ptext="Meça seu CPS e veja sua pontuação!"
+            ptype="small"
+            pimg={urlCPS}
+            picon={iconCPS}
+            pabout="O CPS Test é uma aplicação web que permite ao usuário medir sua velocidade de cliques por segundo e ver sua pontuação. Este projeto foi inspirado em um desafio que meu amigo me propôs, e como sou competitivo, resolvi desenvolvê-lo. Foi uma oportunidade de aprender sobre a manipulação de eventos em JavaScript."
+            pdate="Julho, 2023"
+            psite="https://danielsrbueno.github.io/cps-test/"
+            pgithub="https://github.com/danielsrbueno/cps-test"
+            ptecs={["HTML", "CSS", "JavaScript"]}
+          />
+        </div>
+        <button
+          className="w-40 h-10 rounded-xl after:hover:w-44 self-center outline outline-2 outline-offset-0 outline-rose-600 font-bold text-slate-900 z-50 relative overflow-hidden delay-100 after:w-0 after:h-full after:absolute after:top-0 after:left-0 after:transition-all after:hover:left-0 after:bg-rose-600 hover:delay-100 hover:text-slate-100 after:-z-10 after:rounded-r-full"
+          id="more"
+          onClick={() => {
+            setKey(key ? false : true);
+          }}
+        >
+          <p>Mais projetos</p>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
