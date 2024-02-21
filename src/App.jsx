b6be6e5e4b412d1dd36e005055eca8d63885480e
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2024-02-21 10:52:23 -0300

    add greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useLayoutEffect, useRef } from 'react'
 import { gsap } from 'gsap'
-// import { ScrollSmoother } from 'gsap/ScrollSmoother'
-// import { scrollTrigger } from 'gsap/ScrollTrigger'
 import Navbar from './components/Navbar'
 import Introduction from './components/Introduction'
 import About from './components/About'
@@ -10,13 +8,17 @@ import Contact from './components/Contact'
 
 function App() {
     const comp = useRef(null)
+    let date = new Date()
+    let hour = date.getHours()
+    let greeting = '';
 
-    // gsap.registerPlugin(ScrollSmoother)
-    // ScrollSmoother.create({
-    //     wrapper: '#smooth-wrapper',
-    //     content: '#smooth-content',
-    //     smooth: 2,
-    // })
+    if (hour >= 6 && hour < 12) {
+        greeting = 'Bom dia ☕'
+    } else if (hour >= 12 && hour < 18) {
+        greeting = 'Boa tarde 🍂'
+    } else {
+        greeting = 'Boa noite 🌌'
+    }
 
     useLayoutEffect(() => {
         const introSlider = document.querySelector("#intro-slider")
@@ -37,7 +39,7 @@ function App() {
             }).to(title1, {
                 opacity: 0,
                 y: "-=30",
-                delay: 1,
+                delay: 1, 
                 stagger: 0.5,
             }).to(title2, {
                 opacity: 0,
@@ -56,10 +58,10 @@ function App() {
 
     return (
         <>
-            <div className="w-screen h-screen overflow-x-hidden bg-zinc-100 selection:bg-zinc-800 selection:text-zinc-50 relative" id='smooth-wrapper'>
-                <div className='h-screen p-10 bg-zinc-900 absolute top-0 left-0 z-50 w-full flex flex-col gap-10 tracking-tighter font-black font-jetbrains items-center justify-center  text-zinc-200' id='intro-slider'>
-                    <h1 className='text-5xl' id='title-1'>Esta página está em desenvolvimento...</h1>
-                    <h1 className='text-6xl' id='title-2'>Seja bem-vindo!</h1>
+            <div id="window" className="w-screen h-screen overflow-x-hidden bg-zinc-100 selection:bg-zinc-800 selection:text-zinc-50">
+                <div className='h-screen p-10 bg-zinc-900 absolute top-0 left-0 z-50 w-full flex flex-col gap-4 tracking-tighter font-black font-jetbrains items-center justify-center  text-zinc-200' id='intro-slider'>
+                    <h1 className='max-sm:text-2xl sm:text-5xl lg:text-6xl' id='title-1'>{greeting}</h1>
+                    <h1 className='max-sm:text-2xl sm:text-5xl lg:text-6xl' id='title-2'>Seja bem-vindo!</h1>
                 </div>
                 <Navbar />
                 <Introduction />
