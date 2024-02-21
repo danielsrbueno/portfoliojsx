78b0a2edfe587a052056b6455363a716e3d97f04
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2024-02-21 10:44:55 -0300

    add navbar script

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,59 @@
-import '../index.css'
+import "../index.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/all";
+import { useEffect, useState } from "react";
 
 function Navbar() {
-    return (
-        <nav className="w-screen px-44 py-4 bg-zinc-500/1 fixed backdrop-blur-sm flex items-center justify-around select-none z-40">
-            <h1 className="font-extrabold text-5xl my-gradient-radial font-jetbrains">
-                &lt;Daniel /&gt;
-            </h1>
-            <ul className="font-semibold text-lg flex gap-6 text-zinc-90)">
-                <li><a href="">Início</a></li>
-                <li><a href="">Sobre mim</a></li>
-                <li><a href="">Projetos</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav>
-    )
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollToPlugin);
+
+  const [section, setSection] = useState("");
+
+  useEffect(() => {
+    if (section) {
+      gsap.to("#window", {
+        duration: 0.75,
+        ease: "expo.out",
+        scrollTo: { y: section, offsetY: 80 },
+      });
+    }
+  }, [section]);
+
+  return (
+    <nav className="w-screen px-44 py-4 bg-zinc-500/1 fixed backdrop-blur-sm flex items-center justify-around select-none z-40">
+      <h1 className="font-extrabold text-5xl my-gradient-radial font-jetbrains max-lg:text-4xl">
+        &lt;Daniel /&gt;
+      </h1>
+      <ul className="font-semibold text-lg flex gap-7 text-zinc-600 max-lg:text-base">
+        <li
+          id="link-1"
+          className="cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95"
+          onClick={() => setSection("#introduction")}
+        >
+          Início
+        </li>
+        <li
+          className="cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95"
+          onClick={() => setSection("#about")}
+        >
+          Sobre mim
+        </li>
+        <li
+          className="cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95"
+          onClick={() => setSection("#projects")}
+        >
+          Projetos
+        </li>
+        <li
+          className="cursor-pointer hover:text-zinc-950 transition-all animate-ease border-b-transparent border-b-2 hover:border-zinc-800/95 active:text-zinc-950 active:border-zinc-800/95"
+          onClick={() => setSection("#contact")}
+        >
+          Contato
+        </li>
+      </ul>
+    </nav>
+  );
 }
-    
-        
-export default Navbar
+
+export default Navbar;
