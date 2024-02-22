8747e8926a187d8a048f614d11c208ce9c6134c9
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2024-02-22 10:17:19 -0300

    change main container id

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ function About() {
   const images = ["https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/Arquivo-digitalizado.png", "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/certificado.png", "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/certificate-_1_.png", "https://raw.githubusercontent.com/danielsrbueno/portfolio-imgs/main/certificate.png"];
 
   return (
-    <div className="bg-zinc-400/10 w-full flex flex-col justify-around items-center text-justify my-48 py-12 relative shadow-sm">
+    <div id="about" className="bg-zinc-400/10 w-full flex flex-col justify-around items-center text-justify my-48 py-12 relative shadow-sm">
       <h1 className="mb-6 text-4xl font-bold text-slate-900 before:w-32 before:h-32 before:blur-3xl before:absolute before:translate-x-36 before:-translate-y-10 before:bg-cyan-300/50">
         Sobre mim
       </h1>
@@ -60,6 +60,8 @@ function About() {
               className="self-center outline outline-2 outline-offset-0 outline-rose-600 font-bold text-slate-900 z-50 relative overflow-hidden delay-100 after:w-0 after:h-full after:absolute after:top-0 after:left-0 after:transition-all after:hover:left-0 after:bg-rose-600 hover:delay-100 hover:text-slate-100 after:-z-10 after:rounded-r-full select-none w-36 h-10 rounded-lg after:hover:w-44"
               onClick={() => galleria.current.show()}
             />
+            <Galleria ref={galleria} value={images} 
+       showThumbnails={false}/>
             <Dialog
               className=" bg-slate-100 rounded-lg p-10 shadow-2xl selection:bg-zinc-800 selection:text-zinc-50"
               visible={visible}
@@ -97,8 +99,6 @@ function About() {
                 </div>
               </div>
             </Dialog>
-            <Galleria ref={galleria} value={images} style={{ maxWidth: '50%' }} 
-       showThumbnails={false}/>
           </div>
         </div>
         <div className="before:w-44 before:h-44 before:blur-3xl before:absolute before:translate-x-52 before:translate-y-60 before:bg-amber-300/50">
