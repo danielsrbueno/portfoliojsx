7e836dff5443cd8378aa9a2b3775dca1d48e8a0d
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2024-11-16 15:52:40 -0300

    refactor: change email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,10 +18,10 @@ export default function Contact() {
             <p className="mr-2 mt-0.5">(11) 96053-8467</p>
           </button>
         </a>
-        <a href="mailto:danielsrbueno@gmail.com" target="blank">
+        <a href="mailto:contato@danbueno.com" target="blank">
           <button className="bg-white text-rose-700 w-72 h-12 px-5 flex items-center justify-between rounded-md font-semibold shadow-lg hover:shadow-2xl hover:shadow-rose-700 hover:bg-rose-700 hover:text-white hover:scale-105 transition-all duration-500 max-[320px]:w-52 max-[320px]:text-xs max-[320px]:pr-0 max-[320px]:gap-3">
             <i className="pi pi-envelope text-xl"></i>
-            <p className="mr-2 mt-0.5">danielsrbueno@gmail.com</p>
+            <p className="mr-6 ">contato@danbueno.com</p>
           </button>
         </a>
         <a href="https://www.linkedin.com/in/danielsrbueno/" target="blank">
