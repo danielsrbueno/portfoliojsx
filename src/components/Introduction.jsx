54ed47798d6c1a5b0ac90ad520183959efdc0df1
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2025-10-28 22:29:46 -0300

    feat: automate age changes

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -5,6 +5,10 @@ function Introduction() {
     const urlGithub = "https://github.com/danielsrbueno"
     const urlLinkedin = "https://www.linkedin.com/in/danielsrbueno"
     const [t] = useTranslation("global");
+    
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth() + 1;
+    const age = month > 8 ? year - 2007 : (year - 2007) - 1;
 
     return (
         <div id='introduction' className="w-full h-screen min-h-[560px] flex flex-col justify-center items-center text-center select-none relative">
@@ -15,7 +19,7 @@ function Introduction() {
                 <span>{t("intro.spantitle")}</span>
             </h1>
             <p className="text-xl font-medium text-slate-500 mt-5 max-sm:text-base max-sm:px-3 dark:text-zinc-400">
-            {t("intro.subtitle")}
+            {t("intro.subtitle-st")}{age}{t("intro.subtitle-nd")}
             </p>
             <div className='flex gap-11 mt-6 z-30 max-sm:gap-8'>
                 <a href={urlGithub} target='blank'>
